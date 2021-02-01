dc5a7300e32f0838288804b83a0b07fc588de2cb
Petar Cerovic <quadrition@gmail.com>
2021-02-01 21:57:10 +0100

    Added initial login page

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
+import styles from "./style.module.css";
+
+import LoginArea from "../../templates/login-area/LoginArea";
+
+import {
+  Layout,
+  Input,
+  Button,
+  Row,
+  Col,
+  Image,
+  Typography,
+  notification,
+} from "antd";
+
+const { Content } = Layout;
+
 function LoginPage() {
-  return <React.Fragment>LoginPage</React.Fragment>;
+  return (
+    <Layout className={styles.layout}>
+      <Content>
+        <Row className={styles.rowContent}>
+          <Col sm={12} md={14} lg={16} className={styles.imageArea}></Col>
+          <Col xs={24} sm={12} md={10} lg={8} className={styles.loginArea}>
+            <LoginArea />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
 }
 
 export default LoginPage;
