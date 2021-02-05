0a83db18202dcaf4c7e5f09c71a218809c95e3c6
Petar Cerovic <quadrition@gmail.com>
2021-02-05 12:14:46 +0100

    Added material scroll bar in entire app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./App.css";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import LoginPageContainer from "./modules/login/login-page/LoginPageContainer";
